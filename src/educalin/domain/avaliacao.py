ee57348dd34524cee3d3dbf94099ec63a3729468
Rayan Oliveira <eld.ray.os@outlook.com>
2026-02-03 14:00:28 -0300

    Enhance __init__ docstring in Avaliação class
    Add docstring to the __init__ method of Avaliação class.

--- src/educalin/domain/avaliacao.py
+++ src/educalin/domain/avaliacao.py
@@ -5,6 +5,15 @@ class Avaliacao:
     Representa uma avaliação no sistema, como uma prova ou trabalho.
     """
     def __init__(self, titulo: str, data: date, valor_maximo: float, peso: float):
+        """
+        Cria uma nova instância de Avaliação.
+
+        Args:
+            titulo (str): O titulo da avaliação.
+            data (date): A data de realização da avaliação.
+            valor_maximo (float): O valor máximo a ser atingido na avaliação.
+            peso (float): O peso de cada avaliação.
+        """
         if not isinstance(titulo, str) or not titulo.strip():
             raise ValueError("O título não pode ser vazio.")
 
