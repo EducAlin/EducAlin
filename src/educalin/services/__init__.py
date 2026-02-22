8a41426e9b40600a650a6a5cb9d50d0cf6003c4f
Rayan <eld.ray.os@outlook.com>
2026-02-21 21:03:42 -0300

    feat: adiciona AnaliseFrequencia para identificar baixa frequência de acertos.

--- src/educalin/services/__init__.py
+++ src/educalin/services/__init__.py
@@ -1,4 +1,5 @@
 from educalin.services.estrategiaanalise import EstrategiaAnalise
 from educalin.services.analisenotasbaixas import AnaliseNotasBaixas
+from educalin.services.analisefrequencia import AnaliseFrequencia
 
-__all__ = ['EstrategiaAnalise', 'AnaliseNotasBaixas']
+__all__ = ['EstrategiaAnalise', 'AnaliseNotasBaixas', 'AnaliseFrequencia']
